5464c4fd414b91527ced5d538f8d88ace55ea4b1
llillymarketing <llillymarketing@gmail.com>
2026-03-11 22:01:27 -0500

    fix: src/components/shared/Logo.jsx

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
-export default function Logo({ size = 'default', variant = 'light' }) {
-  const imgSizes = {
-    small: 'h-8',
-    default: 'h-10',
-    large: 'h-14',
-  };
-
+export default function Logo({ size = 'default' }) {
+  const imgSizes = { small: 'h-8', default: 'h-10', large: 'h-14' };
   return (
     <img
       src="https://media.base44.com/images/public/69b1b9537ee76e31a86fda3a/8a7da44ac_GenaraLabsLogo-modified.png"
