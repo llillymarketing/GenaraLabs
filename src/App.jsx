985add3259d781adc9a40b66718ba4378095535a
llillymarketing <llillymarketing@gmail.com>
2026-03-11 22:01:16 -0500

    fix: src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'sonner';
-
 import Layout from './Layout';
 import Home from './pages/Home';
 import Products from './pages/Products';
