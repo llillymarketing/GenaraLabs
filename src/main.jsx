a1e3ced3d07d29c45ab4a1a3c23fd60e06378f20
llillymarketing <llillymarketing@gmail.com>
2026-03-11 22:06:23 -0500

    fix: src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
 import './globals.css';
 
